4d537377695c58e0fd73f691f27f0c5b54a046e2
onapoli <onapoli@protonmail.com>
2020-09-30 10:44:22 +0200

    Removed 'unnecessary' prologue & epilogue from ft_strlen

--- ft_strlen.s
+++ ft_strlen.s
@@ -2,13 +2,11 @@ global ft_strlen
 
 section .text
 ft_strlen:
-	;push r12					prologue
-	mov rax, 0					; init counter
+	mov rax, 0			; init counter
 len_loop:
 	cmp byte [rdi + rax], 0x00	; compare each byte with '\0'
-	je end						; if equal jump to end tag
-	inc rax						; if not equal increment rax
-	jmp len_loop				; continue len_loop
+	je end				; if equal jump to end tag
+	inc rax				; if not equal increment rax
+	jmp len_loop			; continue len_loop
 end:
-	;pop r12					epilogue
 	ret
