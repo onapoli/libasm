2cf28912fd0775d10039b044da5a7528ce5226e1
onapoli <onapoli@protonmail.com>
2020-10-03 16:32:12 +0200

    Added C main for easier testing

---
+++ main.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: onapoli- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/03 15:25:44 by onapoli-          #+#    #+#             */
+/*   Updated: 2020/10/03 16:30:11 by onapoli-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+extern size_t	ft_strlen(char *s);
+extern ssize_t	ft_write(int fd, const void *buf, size_t count);
+extern ssize_t	ft_read(int fd, void *buf, size_t count);
+extern char		*ft_strcpy(char *dest, const char *src);
+extern int		ft_strcmp(const char *s1, const char *s2);
+extern char		*strdup(const char *s);
+
+int main(void)
+{
+	// ft_read VARIABLES
+	int		r_fd;
+	char	*r_file;
+	char	*r_buf;
+	size_t	r_buf_count;
+	// ft_strcpy VARIABLES
+	char	*dest;
+
+	/*
+	** ft_strlen TESTS
+	*/
+	// When NULL is set as argument there's a SEGFAULT error in both ft_strlen & strlen 
+	printf("ft_strlen result: %ld\n", ft_strlen("Hola"));
+	printf("strlen result: %ld\n", strlen("Hola"));
+
+	/*
+	** ft_write TESTS
+	*/
+	printf("ft_write result: ");
+	ft_write(1, "Hola\n", 5);
+	printf("\nwrite result: ");
+	write(1, "Hola\n", 5);
+
+	/*
+	** ft_read TESTS
+	*/
+	r_file = "hello.txt";
+	r_buf_count = 4;
+	if (!(r_buf = malloc(sizeof(char) * r_buf_count)))
+		exit(EXIT_FAILURE);
+	if ((r_fd = open(r_file, O_RDONLY)) < 0)
+		exit(EXIT_FAILURE);
+	printf("\nft_read result: %ld, ft_read_string: %s\n", ft_read(r_fd, r_buf, r_buf_count), r_buf);
+	if (close(r_fd) < 0)
+		exit(EXIT_FAILURE);
+	if ((r_fd = open(r_file, O_RDONLY)) < 0)
+		exit(EXIT_FAILURE);
+	printf("read result: %ld, read_string: %s\n", ft_read(r_fd, r_buf, r_buf_count), r_buf);
+	if (close(r_fd) < 0)
+		exit(EXIT_FAILURE);
+	free(r_buf);
+
+	/*
+	** ft_strcpy TESTS
+	*/
+	if (!(dest = malloc(sizeof(char) * 4 + 1)))
+		exit(EXIT_FAILURE);
+	printf("ft_strcpy result: %s\n", ft_strcpy(dest, "Hola"));
+	printf("strcpy result: %s\n", strcpy(dest, "aloH"));
+	free(dest);
+
+	/*
+	** ft_strcmp TESTS
+	*/
+	printf("ft_strcmp result: %s\n", ft_strcmp("Holas", "Hola"));
+	printf("strcmp result: %s\n", strcmp("Holas", "Hola"));
+
+	/*
+	** ft_strdup TESTS
+	*/
+	return (0);
+}
